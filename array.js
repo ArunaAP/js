f946c7e8a12a703de56f832a4bf4008606eb93a6
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2022-07-26 16:40:15 +0530

    commit

---
+++ array.js
@@ -0,0 +1,17 @@
+let shopping = [];
+shopping = ['paintbrush','colorpalette','canvas'];
+console.log(shopping);
+
+//anothe rexample
+let number = [];
+number = [1,8,6,5,2,9,3,4]
+console.log(number[2] + number[5]) 
+
+console.log(number.sort());
+number.push(7);
+console.log(number.sort());
+
+//example
+let mix=[];
+mix = ['one',1,'two',2];
+console.log(mix);
---
+++ objects.js
@@ -0,0 +1,19 @@
+let pen = {
+    type: 'ballpoint',
+    color: 'black',
+    cost: 10
+};
+//objectname.propertiname
+//objectname['propertiname']
+console.log(pen.color);
+
+//example
+let aruna ={
+    age: 23,
+    university: 'SLIIT',
+    year: '3rd',
+    subject: ['MAD','ITP','PS']
+}
+
+console.log(aruna.university);
+console.log(aruna.subject[2]);
