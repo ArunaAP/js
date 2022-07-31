6792cecc8217517442ec7b8c4da80829ecabb583
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2022-07-31 20:10:42 +0530

    if statement

---
+++ if.js
@@ -0,0 +1,22 @@
+let number=[];
+number=[1,2,1,2,3,2,3,1];
+//if statement
+if(number[0]==number[2]){
+    //console.log('Correct');
+}
+
+//if else statement
+if(number[0]==number[4]){
+    //console.log('Correct')
+}
+else{
+    //console.log('Wrong, Try again')
+}
+
+//example
+if(number[0]==number[2] && number[2]==number[6]){
+    console.log('Correct')
+}
+else{
+    console.log('Wrong, Try again')
+}
