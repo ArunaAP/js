5d910bb1a1e07ea1cc9bed316ce3b33e0ba8f868
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2022-08-02 13:07:14 +0530

    switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+let game= 'football';
+switch(game){
+    case "throwball":
+    console.log("I hate throwball");
+    break;
+    case "bascketball":
+    console.log("bascketball is hard");
+    break;
+    case "cricket":
+    console.log("i love cricket");
+    break;
+    case "football":
+    console.log("i really love football");
+    break;
+    default:
+        console.log("i like every game");
+
+}
---
+++ while.js
@@ -0,0 +1,10 @@
+let i=0;
+//  while(i<5){
+//      console.log("The number is " + i);
+//      i++;
+//  }
+
+do{
+    console.log("Num is " +i);
+    i++;
+}while(i<5);
